902ac530a1b111c1aed1f7954c80b335337984f0
AqingOlin <73264755+AqingOlin@users.noreply.github.com>
2020-12-21 10:55:24 +0800

    button +註解

--- 1221/App.js
+++ 1221/App.js
@@ -4,11 +4,15 @@ import ButtonTwo from './components/BottonTwo'
 
 function App() {
   const [appData, setAppData] = useState('App')
+
+  //準備接收子女的內容狀態用的
   const [childData, setChildData] = useState('')
   return (
     <>
       <h1>App: {childData}</h1>
+      {/*直接特過props傳自己的資料給子女元件*/}
       <ButtonOne textData={appData} />
+      {/*傳一個setter函式給子女元件*/}
       <ButtonTwo setChildData={setChildData} />
     </>
   )
--- 1221/components/BottonTwo.js
+++ 1221/components/BottonTwo.js
@@ -5,6 +5,7 @@ function ButtonTwo(props) {
   return (
     <>
       <h1>ButtonTwo:</h1>
+      {/*利用父母元件傳來的setter函式來傳自己的狀態給父母*/}
       <button onClick={() => props.setChildData(twoData)}>two</button>
     </>
   )
