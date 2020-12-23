de149caa92c079a187cfaa302998cdee8bf880b3
AqingOlin <73264755+AqingOlin@users.noreply.github.com>
2020-12-23 10:54:01 +0800

    ㄎ

---
+++ practice/bootstrap/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+      <button type="button" class="btn btn-secondary">
+        Secondary
+      </button>
+    </>
+  )
+}
+
+export default App
